3a3790846af40d752e639286604226ee20f48b28
Mickaël Andrieu <andrieu.travail@gmail.com>
2018-07-15 02:20:29 +0200

    First commit

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Twig;
+
+use App\Utils\Markdown;
+use Symfony\Component\Intl\Intl;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+/**
+ * This Twig extension adds a new 'md2html' filter to easily transform Markdown
+ * contents into HTML contents inside Twig templates.
+ *
+ * See https://symfony.com/doc/current/cookbook/templating/twig_extension.html
+ */
+class AppExtension extends AbstractExtension
+{
+    private $parser;
+
+    public function __construct(Markdown $parser)
+    {
+        $this->parser = $parser;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('md2html', [$this, 'markdownToHtml'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    /**
+     * Transforms the given Markdown content into HTML content.
+     */
+    public function markdownToHtml(string $content): string
+    {
+        return $this->parser->toHtml($content);
+    }
+}
