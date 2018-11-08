b70256e7f662f5ad209545c8b2b54c1225fc905b
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2018-11-08 09:44:11 +0100

    Startup Script for Codevolve

--- codevolve/start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cp /var/www/html/apache/symfony.conf /etc/apache2/sites-enabled
-sudo service apache2 restart
---
+++ codevolve/startup_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Needs to be copied into "Startup Script" section of Settings.
+if [ -e /var/www/html/codevolve/apache/symfony.conf ]
+then
+    cp /var/www/html/codevolve/apache/symfony.conf /etc/apache2/sites-enabled/
+    rm /etc/apache2/sites-enabled/000-default.conf
+    sudo service apache2 restart
+fi
